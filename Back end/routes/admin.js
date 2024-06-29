b29530b122f5617c63661efa39e58d62ad164b29
John Magbual <jmagbual@Johns-Air.lan>
2024-06-28 20:12:16 -0700

    add post route for adding products

--- Back end/routes/admin.js
+++ Back end/routes/admin.js
@@ -1,14 +1,16 @@
+// routes/admin.js
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
 const verifyAdmin = require('../middlewares/verifyAdmin');
-const { getAllUsers, getAllProducts, addProduct, updateProduct, deleteProduct } = require('../controllers/adminController');
+const { getAllUsers, getAllProducts, getProductById, addProduct, updateProduct, deleteProduct } = require('../controllers/adminController');
 
 router.use(verifyToken);
 router.use(verifyAdmin);
 
 router.get('/users', getAllUsers);
-router.get('/products', getAllProducts);  // This line should already be there
+router.get('/products', getAllProducts);
+router.get('/products/:id', getProductById);  // Ensure this line is here for single product fetching
 router.post('/products', addProduct);
 router.put('/products/:id', updateProduct);
 router.delete('/products/:id', deleteProduct);
