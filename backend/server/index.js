b135fa7ed82e78953aabdd12ce6fd2c6c74bd93f
John Magbual <jmagbual@Johns-Air.hsd1.ca.comcast.net>
2024-07-01 20:26:58 -0700

    Resolved merge conflicts

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,17 +1,19 @@
-// server/index.js
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { connectDB, createTables, insertInitialProducts, insertInitialUsers } = require('./db');
 const adminRoutes = require('../routes/admin');
 const productRoutes = require('../routes/products');
-const cors = require('cors');
+const authRoutes = require('../routes/auth');
+const userRoutes = require('../routes/users');
+const cartRoutes = require('../routes/cart');
+
 const app = express();
 
 console.log('Environment Variables:', process.env);
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const DATABASE_URL = process.env.DATABASE_URL;
-
 
 console.log('JWT_SECRET:', JWT_SECRET);
 console.log('DATABASE_URL:', DATABASE_URL);
@@ -23,20 +25,24 @@ if (!JWT_SECRET) {
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://dancing-bublanina-89ab7a.netlify.app/', // Adjust this to your frontend domain
+  origin: 'http://localhost:3000', // Ensure this matches your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use('/api/admin', adminRoutes);
 console.log('Admin routes loaded');
-app.use('/api/auth', require('../routes/auth'));
+
+app.use('/api/auth', authRoutes);
 console.log('Auth routes loaded');
-app.use('/api/products', productRoutes); // Ensure this line is correct
+
+app.use('/api/products', productRoutes);
 console.log('Product routes loaded');
-app.use('/api/users', require('../routes/users'));
+
+app.use('/api/users', userRoutes);
 console.log('User routes loaded');
-app.use('/api/cart', require('../routes/cart'));
+
+app.use('/api/cart', cartRoutes);
 console.log('Cart routes loaded');
 
 const PORT = process.env.PORT || 3000;
@@ -49,4 +55,3 @@ connectDB().then(async () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
-// trigger rebuild
